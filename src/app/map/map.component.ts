a7ef12316de1f3ddf6949eca8dc71b3acaa0156e
Peltimies <ac9194@student.jamk.fi>
2024-08-19 17:03:47 +0300

    pwa-kartta

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,71 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css'],
+})
+export class MapComponent implements AfterViewInit {
+  private map: any;
+
+  constructor() {}
+  private initMap(): void {
+    // watchPosition-metodi tarkkailee käyttäjän sijaintia ja päivittää sitä mukaan kun käyttäjä liikkuu
+    navigator.geolocation.watchPosition((location) => {
+      const latlng = new L.LatLng(
+        location.coords.latitude,
+        location.coords.longitude
+      );
+
+      const lat = location.coords.latitude;
+
+      this.map = L.map('map').setView(latlng, 13);
+
+      // add the OpenStreetMap tiles
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+      }).addTo(this.map);
+
+      // show the scale bar on the lower left corner
+      L.control.scale().addTo(this.map);
+
+      // show a marker on the map
+      L.marker(latlng).bindPopup('The center of the world').addTo(this.map);
+
+      if (lat > 62) {
+        this.notifyMe();
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  notifyMe() {
+    if (!('Notification' in window)) {
+      // Check if the browser supports notifications
+      alert('This browser does not support desktop notification');
+    } else if (Notification.permission === 'granted') {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification('Hi there!');
+      // …
+    } else if (Notification.permission !== 'denied') {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          const notification = new Notification('Hi there!');
+          // …
+        }
+      });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
+}
